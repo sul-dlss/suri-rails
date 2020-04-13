3b9569c4b6adf0ae23c54d7b5f92a5fbe0fb3a3b
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-04-13 16:56:55 -0700

    Enable recording of deployments via HB

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -5,3 +5,6 @@ server 'sul-suri-prod.stanford.edu', user: 'suri', roles: %w[web app db]
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
 set :bundle_without, %w[deployment test development].join(' ')
+# See https://github.com/honeybadger-io/honeybadger-ruby/issues/129 &
+# https://github.com/honeybadger-io/honeybadger-ruby/blob/7eea24a47d44aed663e315be970e501b7cf092fc/vendor/capistrano-honeybadger/README.md
+set :honeybadger_server, primary(:app)
