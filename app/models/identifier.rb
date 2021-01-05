1c63acd85f930b8fb6172b571381a04be910db02
Naomi Dushay <ndushay@stanford.edu>
2021-01-05 12:25:06 -0800

    avoid deprecation messages for using return in transaction block

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -9,13 +9,16 @@ class Identifier < ApplicationRecord
   # @return [Identifier] return an identifier instance if no errors
   # @raise [RuntimeError,ActiveRecord::ActiveRecordError] if minting encounters errors
   def self.mint(attributes = {})
+    identifier = nil
+
     MAX_RETRIES.times do
       id = DruidMinter.generate
       Identifier.transaction do
         next if Identifier.where(identifier: id).any?
 
-        return Identifier.create!(attributes.merge(identifier: id))
+        identifier = Identifier.create!(attributes.merge(identifier: id))
       end
+      return identifier if identifier.present?
     end
 
     raise "Tried to mint a unique identifier #{MAX_RETRIES} times and failed."
