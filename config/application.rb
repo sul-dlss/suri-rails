c420e7c96317972d542d76b5be919f2c7e5288bd
Justin Littman <justinlittman@stanford.edu>
2024-10-28 14:12:25 -0400

    Upgrade to Rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ end
 module Suri
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Stay out of business of validating gem-provided routes (e.g., OKComputer)
     accept_proc = proc { |request| request.path.include?('/identifiers') }
